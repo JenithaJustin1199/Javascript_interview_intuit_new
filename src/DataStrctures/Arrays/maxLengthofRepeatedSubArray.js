cc09a1426af6fa73738dad310b420ecb52eee547
JenithaJustin1199 <119147818+JenithaJustin1199@users.noreply.github.com>
2024-01-04 17:48:52 +0530

    Update maxLengthofRepeatedSubArray.js

--- src/DataStrctures/Arrays/maxLengthofRepeatedSubArray.js
+++ src/DataStrctures/Arrays/maxLengthofRepeatedSubArray.js
@@ -14,4 +14,27 @@ function maxLengthOfRepeatedSubarray(arr1, arr2) {
     }
   }
   return maxlength;
+}O(n*m)
+function maxLengthOfRepeatedSubarray(arr1, arr2) {
+    let m = arr1.length;
+    let n = arr2.length;
+    
+    let maxLength = 0;
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            let length = 0;
+
+            // Iterate to find the common subarray length
+            while (i + length < m && j + length < n && arr1[i + length] === arr2[j + length]) {
+                length++;
+            }
+
+            // Update maxLength if the current common subarray is longer
+            maxLength = Math.max(maxLength, length);
+        }
+    }
+
+    return maxLength;
 }
+O(n*3)
