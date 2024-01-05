68934319e497215c31858bf6eb7fce2adce1d42c
JenithaJustin1199 <119147818+JenithaJustin1199@users.noreply.github.com>
2024-01-05 09:12:14 +0530

    Update subArrayswithsum.js

--- src/DataStrctures/Arrays/subArrayswithsum.js
+++ src/DataStrctures/Arrays/subArrayswithsum.js
@@ -30,3 +30,39 @@ function subArraywithSum(arr, targetSum) {
   // Return the result array containing subarrays with the target sum
   return result;
 }
+
+// count 
+function countSubArrayswithSum(arr, targetSum) {
+  // Initialize pointers and sum
+  let left = 0;
+  let right = 0;
+  let sum = arr[left];
+
+  // Initialize count to store the number of subarrays with the target sum
+  let count = 0;
+
+  // Iterate through the array using two pointers
+  while (left <= right && right < arr.length) {
+    // If the current sum equals the target sum, increment the count
+    if (sum === targetSum) {
+      count++;
+      right++;
+      sum += arr[right];
+    }
+    // If the current sum is less than the target sum, move the right pointer to the right
+    else if (sum < targetSum) {
+      right++;
+      sum += arr[right];
+    }
+    // If the current sum is greater than the target sum, move the left pointer to the right
+    else {
+      sum -= arr[left];
+      left++;
+    }
+  }
+
+  // Return the count of subarrays with the target sum
+  return count;
+}
+
+Time compl - O(N)
