1e1ccefad972a537d257455106d7b50e5c025acc
JenithaJustin1199 <119147818+JenithaJustin1199@users.noreply.github.com>
2024-01-05 09:41:07 +0530

    Update BasicDriverCode.js

--- src/DataStrctures/LinkedList/BasicDriverCode.js
+++ src/DataStrctures/LinkedList/BasicDriverCode.js
@@ -4,17 +4,20 @@ export class Node {
     this.next = null;
   }
 }
+
 export class LinkedList {
   constructor() {
     this.head = null;
     this.size = 0;
   }
+
   isEmpty() {
     return this.size === 0;
   }
+
   append(value) {
     let newNode = new Node(value);
-    if (this.isEmpty) {
+    if (this.isEmpty()) {
       this.head = newNode;
     } else {
       newNode.next = this.head;
@@ -22,9 +25,10 @@ export class LinkedList {
     }
     this.size++;
   }
+
   prepend(value) {
     let newNode = new Node(value);
-    if (this.isEmpty) {
+    if (this.isEmpty()) {
       this.head = newNode;
     } else {
       let prev = this.head;
@@ -35,17 +39,47 @@ export class LinkedList {
     }
     this.size++;
   }
-  print(value) {
-    if (this.isEmpty) {
+
+  print() {
+    if (this.isEmpty()) {
       console.log("Empty LinkedList");
     } else {
       let curr = this.head;
-      let value = " ";
+      let result = " ";
       while (curr) {
-        value += `${curr.value}`;
+        result += `${curr.value} `;
         curr = curr.next;
       }
-      console.log(value);
+      console.log(result);
     }
   }
+
+  reverse() {
+    let prev = null;
+    let current = this.head;
+    let next = null;
+
+    while (current) {
+      next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+    }
+
+    this.head = prev;
+  }
 }
+
+// Example usage:
+const linkedList = new LinkedList();
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+
+console.log("Original LinkedList:");
+linkedList.print();
+
+linkedList.reverse();
+
+console.log("Reversed LinkedList:");
+linkedList.print();
