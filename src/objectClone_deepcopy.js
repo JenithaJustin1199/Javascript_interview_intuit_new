c8f7ce0e68fa4fce7ea1d14428b8a885900cb8c3
JenithaJustin1199 <119147818+JenithaJustin1199@users.noreply.github.com>
2024-01-28 19:25:54 +0530

    Update objectClone_deepcopy.js

--- src/objectClone_deepcopy.js
+++ src/objectClone_deepcopy.js
@@ -35,4 +35,56 @@ const originalObject = { key1: 'value1', key2: 'value2' };
 const clonedObject = Object.create(originalObject);
 Note: This method creates a new object with the original object as its prototype. It may not work as expected for certain use cases.
 
-Choose the method that best fits your requirements and the complexity of your object. The first two methods are commonly used for shallow cloning, while the third method is used for deep cloning. If your object contains nested objects or functions, it's advisable to use a deep cloning method, like Object.assign or the spread operator. If you're working with complex data structures, consider using a library like Lodash for more comprehensive cloning options.
+Choose the method that best fits your requirements and the complexity of your object. 
+  The first two methods are commonly used for shallow cloning, while the third method is used for deep cloning.
+  If your object contains nested objects or functions, it's advisable to use a deep cloning method,
+  like Object.assign or the spread operator. If you're working with complex data structures,
+  consider using a library like Lodash for more comprehensive cloning options.
+
+
+  function deepCopy(obj) {
+  const jsonString = JSON.stringify(obj);
+  const copy = JSON.parse(jsonString);
+  return copy;
+}
+
+// Step 2: Start with a Shallow Copy
+function shallowCopy(obj) {
+    return { ...obj };
+  }
+  
+  // Step 3: Recursive Deep Copy
+  function deepCopy(obj) {
+    // Check if obj is an object or array
+    if (typeof obj !== 'object' || obj === null) {
+      return obj; // Return non-objects as is
+    }
+  
+    // Create a new object or array
+    const copy = Array.isArray(obj) ? [] : {};
+  
+    // Iterate over the properties or elements and recursively deep copy them
+    for (const key in obj) {
+      if (Object.hasOwnProperty.call(obj, key)) {
+        copy[key] = deepCopy(obj[key]);
+      }
+    }
+  
+    return copy;
+  }
+  
+  // Step 4: Test the Deep Copy Function
+  const originalObject = {
+    name: 'John',
+    age: 30,
+    address: {
+      city: 'New York',
+      country: 'USA',
+    },
+    hobbies: ['reading', 'coding'],
+  };
+  
+  const copiedObject = deepCopy(originalObject);
+  
+  console.log(copiedObject);
+  
